46470515e264aca79e8996973e33394f2b710548
Yuri <yurkor4@gmail.com>
2025-02-14 14:43:31 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,41 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"regexp"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func isDigit(r rune) bool {
+	re := regexp.MustCompile(`\d`)
+	return re.MatchString(string(r))
+}
+
+func Unpack(input string) (string, error) {
+	if input == "" {
+		return "", nil
+	}
+	sliceStr := []rune(input)
+	var result string
+	var skip int
+
+	for i, v := range sliceStr {
+
+		if i == 0 && isDigit(v) {
+			return "", ErrInvalidString
+		}
+
+		if i+1 < len(sliceStr) && isDigit(sliceStr[i+1]) {
+			multi := int(sliceStr[i+1] - '0')
+			skip = i + 1
+
+			for j := 0; j < multi; j++ {
+				result += string(v)
+			}
+		} else if i != skip {
+			result += string(v)
+		}
+	}
+
+	return result, nil
 }
